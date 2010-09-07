b2996500a24392208a1e2509ad736b18adaeeb44
Michael Brooks <michael@michaelbrooks.ca>
2010-09-06 20:49:43 -0700

    Refactor phonegap.js for cleanliness.

--- js/phonegap.js
+++ js/phonegap.js
@@ -1,13 +1,56 @@
-PhoneGap = { };
-navigator = { };
+// PHONEGAP
+
+var PhoneGap = {
+    callbackId: 0,
+    callbacks: {},
+
+    klassMap: {
+        'com.phonegap.Network':      'com.phonegap.network.Network',
+        'com.phonegap.Notification': 'com.phonegap.notification.Notification'
+    },
+
+    exec: function(success, fail, klass, action, args) {
+        klass = this.klassMap[klass];
+
+        var callbackId = klass + PhoneGap.callbackId++;
+        PhoneGap.callbacks[callbackId] = { success:success, fail:fail };
+
+        return phonegap.commandManager.exec(klass, action, callbackId, JSON.stringify(args));
+    },
+
+    execSync: function(klass, action, args) {
+        klass = this.klassMap[klass];
+
+        return phonegap.commandManager.exec(klass, action, null, JSON.stringify(args));
+    },
+
+    callbackSuccess: function(callbackId, args) {
+        PhoneGap.callbacks[callbackId].success(args.message);
+        PhoneGap.clearExec(callbackId);
+    },
+
+    callbackError: function(callbackId, args) {
+        PhoneGap.callbacks[callbackId].fail(args);
+        PhoneGap.clearExec(callbackId);
+    },
+
+    clearExec: function(callbackId) {
+        delete PhoneGap.callbacks[callbackId];
+    }
+};
+
+// DEVICE
 
 PhoneGap.Device = {
-  // platform: phonegap.device.platform,
-  // version:  blackberry.system.softwareVersion,
-  // name:     blackberry.system.model,
-  // uuid:     phonegap.device.uuid
+  platform: phonegap.device.platform,
+  version:  blackberry.system.softwareVersion,
+  name:     blackberry.system.model,
+  uuid:     phonegap.device.uuid
 };
+
 window.device = navigator.device = PhoneGap.Device;
+
+// NOTIFICATION
 
 PhoneGap.Notification = {
   vibrate: function(duration) {
@@ -17,13 +60,15 @@ PhoneGap.Notification = {
   beep: function(count) {
     PhoneGap.execSync('com.phonegap.Notification', 'beep', [count]);
   },
-  
+
   alert: function(message, title, buttonLabel) {
     PhoneGap.execSync('com.phonegap.Notification', 'alert', [message, title, buttonLabel]);
   }
 };
 
 navigator.notification = PhoneGap.Notification;
+
+// NETWORK
 
 NetworkStatus = {
   NOT_REACHABLE: 0,
@@ -38,52 +83,3 @@ PhoneGap.Network = {
 };
 
 navigator.network = PhoneGap.Network;
-
-/* ----- phonegap.blackberry-widgets.js ------ */
-
-PhoneGap.EXEC_SYNC  = 0;
-PhoneGap.EXEC_ASYNC = 1;
-
-PhoneGap.callbackId = 0;
-PhoneGap.callbacks  = {};
-
-PhoneGap.resolveKlass = function(klass) {
-    if (klass.toLowerCase() === 'com.phonegap.notification') {
-        klass = 'com.phonegap.notification.Notification';
-    }
-    else if (klass.toLowerCase() === 'com.phonegap.network') {
-        klass = 'com.phonegap.network.Network';
-    }
-    
-    return klass;
-}
-
-PhoneGap.exec = function(success, fail, klass, action, args) {
-    klass = PhoneGap.resolveKlass(klass);
-    
-    var callbackId = klass + PhoneGap.callbackId++;
-    
-    PhoneGap.callbacks[callbackId] = { success:success, fail:fail };
-
-    return phonegap.commandManager.exec(klass, action, callbackId, JSON.stringify(args));
-}
-
-PhoneGap.callbackSuccess = function(callbackId, args) {
-    PhoneGap.callbacks[callbackId].success(args.message);
-    PhoneGap.clearExec(callbackId);
-};
-
-PhoneGap.callbackError = function(callbackId, args) {
-    PhoneGap.callbacks[callbackId].fail(args);
-    PhoneGap.clearExec(callbackId);
-};
-
-PhoneGap.clearExec = function(callbackId) {
-    delete PhoneGap.callbacks[callbackId];
-};
-
-PhoneGap.execSync = function(klass, action, args) {
-    klass = PhoneGap.resolveKlass(klass);
-    
-    return phonegap.commandManager.exec(klass, action, null, JSON.stringify(args), PhoneGap.EXEC_SYNC);
-}
